a8c40be78188068f771b456d221c7118ab7e3d88
Alperen <alperen.talaslioglu@gmail.com>
2015-08-23 00:11:36 +0300

    Model Update

--- src/model/SplitterModel.java
+++ src/model/SplitterModel.java
@@ -1,5 +1,20 @@
 package model;
 
-public class SplitterModel {
+import java.io.IOException;
+import java.util.List;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.Splitter;
+
+public class SplitterModel {
+	private List<PDDocument> listOfSplitPages;
+
+	public SplitterModel(PDDocument document) throws IOException {
+		Splitter splitter = new Splitter();
+		listOfSplitPages = splitter.split(document);
+	}
+
+	public List<PDDocument> getDocument() {
+		return listOfSplitPages;
+	}
 }
